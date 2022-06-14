ab547e1131f7c47c3bad1db8cb82cf16df75dc90
Xaypanya Phongsa <dackogan@gmail.com>
2022-06-14 23:34:48 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans Lao', 'sans-serif'],
+        pops: ['Poppins', 'cursive']
+      },
+    }
+  },
+  plugins: [
+    require('preline/plugin')
+  ],
+}
